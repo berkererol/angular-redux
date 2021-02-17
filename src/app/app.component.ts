b33ec5c03e524b86bb4bca48ae2fbdfb54acf3e5
Berker Erol <berker.erol@onespan.com>
2021-02-17 15:35:24 -0500

    counter with redux store.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
 import { Component } from '@angular/core';
 
+import { NgRedux, select } from '@angular-redux/store';
+import { CounterActions } from './app.actions';
+import { IAppState } from '../store';
+import { Observable } from 'rxjs';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-redux';
+  title = 'app works!';
+  @select()
+  readonly count$: Observable<number>;
+  subscription;
+
+  constructor(
+    private ngRedux: NgRedux<IAppState>,
+    private actions: CounterActions
+  ) { }
+
+
+  increment() {
+    this.ngRedux.dispatch(this.actions.increment());
+  }
+  decrement() {
+    this.ngRedux.dispatch(this.actions.decrement());
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,32 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
+import { NgReduxModule, NgRedux } from '@angular-redux/store';
+
+import { rootReducer, IAppState, INITIAL_STATE } from '../store';
+import { CounterActions } from './app.actions';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    NgReduxModule,
   ],
-  providers: [],
+  providers: [CounterActions],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+  constructor(ngRedux: NgRedux<IAppState>) {
+    ngRedux.configureStore(
+      rootReducer,
+      INITIAL_STATE);
+  }
+
+}
